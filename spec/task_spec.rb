b21fba91cd892a0cc2a54466a1625cacefc574ef
Debbie <debbalbo@hotmail.com>
2015-06-08 14:23:49 -0700

    changed all spec file requirements to spec helper file

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,15 +1,4 @@
-require('rspec')
-require('task')
-require('pg')
-require('pry')
 
-DB = PG.connect({:dbname => 'to_do_test'})
-
-RSpec.configure do |config|
-	config.after(:each) do
-		DB.exec('DELETE FROM tasks *;')
-	end
-end
 
 
 		describe(Task) do
